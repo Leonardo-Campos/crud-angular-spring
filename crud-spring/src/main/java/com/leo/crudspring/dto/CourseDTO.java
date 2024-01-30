7eb4913b0b6fbd5e0e530712cea11348a44457d5
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-01-29 21:19:40 -0300

    MySQL + Spring Docker Compose

--- crud-spring/src/main/java/com/leo/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/leo/crudspring/dto/CourseDTO.java
@@ -3,8 +3,6 @@ package com.leo.crudspring.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.leo.crudspring.enums.Category;
 import com.leo.crudspring.enums.validation.ValueOfEnum;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -15,7 +13,7 @@ import java.util.List;
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length(max = 10) @Enumerated(EnumType.STRING) String category,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
 
